6cf488fdcfd5099cec101f47b67f00ca56b14080
Justin Welsh <justinwelsh33@gmail.com>
2022-07-01 13:25:59 -0500

    First commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,30 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nEnter a something and I will tell you if it is a number.");
+        String isNumInput = sc.nextLine();
+        System.out.println(StringUtils.isNumeric(isNumInput));
+
+        System.out.println("\nEnter some text and I will swap its character case from lower to upper and vise versa.");
+        String strToSwap = sc.nextLine();
+        System.out.println(StringUtils.swapCase(strToSwap));
+
+        System.out.println("\nEnter some text and I will reverse it.");
+        String strToReverse = sc.nextLine();
+        System.out.println(StringUtils.reverse(strToReverse));
+
+
+        //todo: 1. Tells whether or not what the user entered is a number
+        //      2. Flips the case of the string
+        //      3. Reverses the string
+
+//        System.out.println(StringUtils.isNumeric("33")); // returns a boolean.
+//        System.out.println(StringUtils.swapCase("Hello World")); // returns string in reverse case.
+//        System.out.println(StringUtils.reverse("Hello World")); // returns string in reverse.
+    }
+}
